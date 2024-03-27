47a0b5918f05ef96463ee6f1d1ed2af5de99adac
62savage <6s2avage@gmail.com>
2024-03-28 00:06:21 +0900

    도시별 날씨 가져오기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import WeatherButton from "./components/WeatherButton";
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState(null);
   const cities = ["paris", "new york", "tokyo", "seoul"];
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -22,15 +23,26 @@ function App() {
     setWeather(data);
   };
 
+  const getWeatherByCityName = async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
+
   useEffect(() => {
-    getCurrentLocation();
-  }, []);
+    if (city == "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCityName();
+    }
+  }, [city]);
 
   return (
     <>
       <div className="container">
         <WeatherBox weather={weather} />
-        <WeatherButton setWeather={setWeather} cities={cities} />
+        <WeatherButton setCity={setCity} cities={cities} />
       </div>
     </>
   );
--- src/components/WeatherButton.js
+++ src/components/WeatherButton.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-export default function WeatherButton({ setWeather, cities }) {
+export default function WeatherButton({ setCity, cities }) {
   return (
-    <>
+    <div>
       <Button variant="warning">Current Location</Button>
-      {cities.map((city) => (
-        <Button variant="warning">{city}</Button>
+      {cities.map((city, index) => (
+        <Button
+          variant="warning"
+          key={index}
+          onClick={() => {
+            setCity(city);
+          }}
+        >
+          {city}
+        </Button>
       ))}
-    </>
+    </div>
   );
 }
