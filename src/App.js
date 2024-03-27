0952d8d42d88d15e0b24f4d690278d6d9ae758d1
62savage <6s2avage@gmail.com>
2024-03-27 21:50:58 +0900

    마운트 되자마자 현재위치 기반 날씨 가져오기

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ function App() {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      console.log(lat, lon);
+      getWeatherByCurrentLocation(lat, lon);
     });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
   };
 
   useEffect(() => {
